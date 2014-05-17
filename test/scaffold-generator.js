1ef3273eb79dc4698f96cbc47eeaa550a7b532a0
kaelzhang <i@kael.me>
2014-05-17 22:14:38 +0800

    test: fixes default ejs template; #1

--- test/scaffold-generator.js
+++ test/scaffold-generator.js
@@ -10,26 +10,48 @@ var fixtures = node_path.join(__dirname, 'fixtures');
 var expected = node_path.join(fixtures, 'expected');
 
 function expect_file (to, expected, name, expect_name) {
-  var content = fs.read( node_path.join(expected, name) );
+  var content = fs.read( node_path.join(to, name) );
   var expect_content = fs.read( node_path.join(expected, expect_name || name) );
    expect(content).to.equal(expect_content);
 }
 
 describe("scaffold-generator", function(){
-  it(".generate(from, to, callback)", function(done){
+  it(".generate(from, to, callback), override=false", function(done){
     var to = tmp.make(fixtures);
     var from = node_path.join(fixtures, 'template');
-    
+    fs.write( node_path.join(to, 'lib/index.js'), 'abc');
 
     scaffold({
       data: {
         name: 'cortex',
-        main: ''
+        main: 'lib/index.js'
       }
 
     }).generate(from, to, function (err) {
-      expect_file(to, expected, 'index.js');
       expect_file(to, expected, 'package.json');
+
+      // no change
+      expect( fs.read(node_path.join(to, 'lib/index.js')) ).to.equal('abc');
+      expect(err).to.equal(null);
+      done();
+    });
+  });
+
+  it(".generate(from, to, callback), override=true", function(done){
+    var to = tmp.make(fixtures);
+    var from = node_path.join(fixtures, 'template');
+    fs.write( node_path.join(to, 'lib/index.js'), 'abc');
+
+    scaffold({
+      data: {
+        name: 'cortex',
+        main: 'lib/index.js'
+      },
+      override: true
+
+    }).generate(from, to, function (err) {
+      expect_file(to, expected, 'package.json');
+      expect_file(to, expected, 'lib/index.js', 'index.js');
       expect(err).to.equal(null);
       done();
     });
